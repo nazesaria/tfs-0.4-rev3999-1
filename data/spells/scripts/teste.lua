65ce491dfa032691770f5c4d1fbad66247fd348d
Naze <nazesaria@live.com>
2020-09-10 22:45:45 -0300

    atualizações

--- data/spells/scripts/teste.lua
+++ data/spells/scripts/teste.lua
@@ -1,22 +1,25 @@
 local combat50_Brush = createCombatObject()
 setCombatParam(combat50_Brush, COMBAT_PARAM_EFFECT, CONST_ME_MORTAREA)
 setCombatParam(combat50_Brush, COMBAT_PARAM_TYPE, COMBAT_DEATHDAMAGE)
-setAttackFormula(combat, COMBAT_FORMULA_LEVELMAGIC, 5, 5, 1.4, 2.8, 40, 70)
 setCombatArea(combat50_Brush,createCombatArea({
-{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0},
-{0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-{0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-{1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1},
-{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-{0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-{0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-{0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-}))
+	{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+	{0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+	{0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+	{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+	{1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1},
+	{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+	{0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+	{0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+	{0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+	}))
+function getDmg_Brush(cid, level, maglevel)
+	return (10)*-1,(10)*-1 
+end
+setCombatCallback(combat50_Brush, CALLBACK_PARAM_LEVELMAGICVALUE, "getDmg_Brush")
 
 local function RunPart(c,cid,var)
 	if (isCreature(cid)) then
